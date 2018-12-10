39f91bde634d1ae9170b05356438f3618a860e73
airqj <gugeqinjianbo@gmail.com>
2018-12-10 23:51:19 +0800

    do some modify

--- fitnew.lua
+++ fitnew.lua
@@ -3,60 +3,15 @@ local util = require "luci.util"
 local ltn12 = require "luci.ltn12"
 local cjson = require "cjson"
 local sys   = require("luci.sys")
+local http=require("socket.http")
 --local ap    = require("ap")
 
-function table2json(t)  
-        local function serialize(tbl)  
-                local tmp = {}  
-                for k, v in pairs(tbl) do  
-                        local k_type = type(k)  
-                        local v_type = type(v)  
-                        local key = (k_type == "string" and "\"" .. k .. "\":")  
-                            or (k_type == "number" and "")  
-                        local value = (v_type == "table" and serialize(v))  
-                            or (v_type == "boolean" and tostring(v))  
-                            or (v_type == "string" and "\"" .. v .. "\"")  
-                            or (v_type == "number" and v)  
-                        tmp[#tmp + 1] = key and value and tostring(key) .. tostring(value) or nil  
-                end  
-                if table.maxn(tbl) == 0 then  
-                        return "{" .. table.concat(tmp, ",") .. "}"  
-                else  
-                        return "[" .. table.concat(tmp, ",") .. "]"  
-                end  
-        end  
-        assert(type(t) == "table")  
-        return serialize(t)  
-end  
 
-function disassociate(mac)
-  syslog("disassociate: "..mac)
-	local cmd_disassociate = "hostapd_cli -i wlan0 disassociate "..mac
-	local result = sys.exec(cmd_disassociate)
-  if(result == "OK\n") then
-    return cjson.encode({errcode=0})
-  else
-    return cjson.encode({errcode=result})
-  end
-end
 
-function add_mac_to_blacklist(mac)
-    syslog("add mac to blacklist: "..mac)
-    local file_path = "/var/run/hostapd_fitnew_maclist.conf"
-    local file = io.open(file_path,"a")
-    io.output(file)
-    io.write(mac.."\n")
-    io.close(file)
-end
 
-function remove_mac_from_blacklist(mac)
-    local file_maclist = "/var/run/hostapd_fitnew_maclist.conf"
-    local cmd_remove_mac_from_black_list_prefix = "sed -i '/%s.*/d' %s"
-    local cmd_remove_mac_from_black_list = string.format(cmd_remove_mac_from_black_list_prefix,mac,file_maclist)
-    syslog(cmd_remove_mac_from_black_list)
-    local result = sys.call(cmd_remove_mac_from_black_list)
-    return result
-end
+
+
+
 
 function syslog(log)
 	os.execute(string.format([[logger "%s"]],log))
@@ -67,114 +22,53 @@ function os.capture(cmd)
 	return s
 end
 
---cmd_redirect = "ebtables -t nat -I PREROUTING -s %s -p ipv4 --ip-proto udp --ip-dst 239.255.255.250 --ip-dport 1900 -j dnat --to-destination %s"
-cmd_redirect = "ebtables -t nat -I PREROUTING -s %s -p ipv4 --ip-proto udp --ip-dport 1900 -j dnat --to-destination %s && \
-                ebtables -t nat -I PREROUTING -s %s -p ipv4 --ip-proto udp --ip-dport 5353 -j dnat --to-destination %s"
---cmd_accept   = "ebtables -I FORWARD -s %s -d %s -p ipv4 --ip-proto udp --ip-dport 1900 -j ACCEPT && \
---                ebtables -I FORWARD -s %s -d %s -p ipv4 --ip-proto udp --ip-dport 5353 -j ACCEPT"
-cmd_accept = "ebtables -I FORWARD -p ipv4 --ip-proto tcp -s %s -d %s -j ACCEPT"
-cmd_prefix = "cat /tmp/dhcp.leases | grep %s | awk '{print $2}'"
-
---cmd_rm_redirect = "ebtables -t nat -D PREROUTING -s %s -p ipv4 --ip-proto udp --ip-dst 239.255.255.250 --ip-dport 1900 -j dnat --to-destination %s"
-cmd_rm_redirect = "ebtables -t nat -D PREROUTING -s %s -p ipv4 --ip-proto udp --ip-dport 1900 -j dnat --to-destination %s && \
-                   ebtables -t nat -D PREROUTING -s %s -p ipv4 --ip-proto udp --ip-dport 5353 -j dnat --to-destination %s"
---cmd_rm_accept   = "ebtables -D FORWARD -s %s -d %s -p ipv4 --ip-proto udp --ip-dport 1900 -j ACCEPT && \
---                   ebtables -D FORWARD -s %s -d %s -p ipv4 --ip-proto udp --ip-dport 5353 -j ACCEPT"
-
-cmd_rm_accept = "ebtables -D FORWARD -p ipv4 --ip-proto tcp -s %s -d %s -j ACCEPT"
-
-function get_mac_by_ip(ip)
-	local cmd_get_mac_by_ip = string.format(cmd_prefix,ip)
-	local mac = sys.exec(cmd_get_mac_by_ip)
-  return string.gsub(mac,"\n","")
-end
-
-function pre_build_rule(mac)
-  local cmd_pre_build_rule = "/root/ebtables.sh "..mac
-  syslog("pre_build_rule: "..cmd_pre_build_rule)
-  sys.call(cmd_pre_build_rule)
-end
-
-function build_rule(mac_client,mac_server)
-	--local cmd_redirect_client2server = string.format(cmd_redirect,mac_client,mac_server,mac_client,mac_server)
-	--local cmd_redirect_server2client = string.format(cmd_redirect,mac_server,mac_client,mac_server,mac_client)
-  pre_build_rule(mac_client)
-  pre_build_rule(mac_server)
-	local cmd_accept_client2server   = string.format(cmd_accept,mac_client,mac_server)
-	local cmd_accept_server2client   = string.format(cmd_accept,mac_server,mac_client)
-
-	local table_cmd_build = {cmd_accept_client2server,cmd_accept_server2client}
-	for i=1,table.getn(table_cmd_build) do
-		local result = sys.call(table_cmd_build[i])
-		if(result ~= 0) then
-			return cjson.encode({errcode=1,cmd=table_cmd_build[i]})
-		end
-	end
-  return 0
-end
-
-function rm_rule(mac_client,mac_server)
-	--local cmd_rm_redirect_client2server = string.format(cmd_rm_redirect,mac_client,mac_server,mac_client,mac_server)
-	--local cmd_rm_redirect_server2client = string.format(cmd_rm_redirect,mac_server,mac_client,mac_server,mac_client)
-	local cmd_rm_accept_client2server   = string.format(cmd_rm_accept,mac_client,mac_server)
-	local cmd_rm_accept_server2client   = string.format(cmd_rm_accept,mac_server,mac_client)
+function request_server(server,client,token)
+   syslog("server="..server)
+   syslog("client="..client)
+   syslog("token="..token)
+   local request_body_prefix = [[
+		<?xml version="1.0" encoding="utf-8"?>
+    <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">  
+      <s:Body> 
+        <u:SetDeviceAuthorization xmlns:u="urn:schemas-upnp-org:service:ConnectionManager:1">  
+          <DeviceIP>%s</DeviceIP>  
+          <Token>%s</Token>  
+          <Enable>1</Enable> 
+        </u:SetDeviceAuthorization> 
+      </s:Body> 
+    </s:Envelope>
+    ]]
   
-	local table_cmd_rm = {cmd_rm_accept_client2server,cmd_rm_accept_server2client}
-	for i=1,table.getn(table_cmd_rm) do
-		local result = sys.call(table_cmd_rm[i])
-		if(result ~= 0) then
-			syslog("call error: "..table_cmd_rm[i])
-		end
-	end
-end
+  local request_body = string.format(request_body_prefix,client,token)
+  
+  --syslog(request_body)
+  
+  local response_body = {}
 
-function delete_userid_entry(userid)
-  local record_userid_servermac_file = "/var/run/fitnew_userid_mac"
-  local cmd_delete_userid_entry_prefix = "sed -i '/^%s/d' %s"
-  local cmd_delete_userid_entry = string.format(cmd_delete_userid_entry_prefix,userid,record_userid_servermac_file)
-  local result = sys.call(cmd_delete_userid_entry)
-  return  cjson.encode({errcode=result})
-end
+  local res, code, response_headers = http.request{
+      url = server.."/_urn:schemas-upnp-org:service:ConnectionManager_control",
+      method = "POST",
+      headers =
+        {
+            ["SOAPACTION"] = [["urn:schemas-upnp-org:service:ConnectionManager:1#SetDeviceAuthorization"]];
+            ["Content-Type"] = [[text/xml]];
+            ["Content-Length"] = #request_body;
+        },
+        source = ltn12.source.string(request_body),
+        sink = ltn12.sink.table(response_body),
+  }
 
-function record_userid_servermac_to_file(userid,server_mac)
-  delete_userid_entry(userid) -- 写之前先将同一userid的记录删掉
-  local record_userid_servermac_file = "/var/run/fitnew_userid_mac"
-  local line = string.format("%s;;%s",userid,server_mac)
-  local file = io.open(record_userid_servermac_file,"a")
-  io.output(file)
-  local result = io.write(line.."\n")
-  io.close(file)
-  if(result == true) then
-    return 0
-  else
-    return result
-  end
-end
-
-
-function update_record_file(userid,mac_client,mac_server)
-  local record_userid_server_mac_file = "/var/run/fitnew_userid_mac"
-  local record_new = string.format("%s;%s;%s",userid,mac_client,mac_server)
-  local cmd_replace_record_prefix = "sed -i 's/^%s.*/%s/g' %s"
-  local cmd_replace_record = string.format(cmd_replace_record_prefix,userid,record_new,record_userid_server_mac_file)
-  syslog("sed command: "..cmd_replace_record)
-  local result = sys.exec(cmd_replace_record)
-  return result
+  --syslog("response_headers.length="..response_headers.content-length)
+  return code
 end
 
 function parse_data_from_phone(data,env)
   syslog("type=1")
   local ip_client = env.REMOTE_HOST
-	local mac_client = get_mac_by_ip(ip_client)
-  local userid    = data.userid
-  local mac_server = get_server_mac_from_record_file_by_userid(userid)
-  local result = build_rule(mac_client,mac_server)  -- 实际为mac_client,为了方便测试，改为data.client
-  update_record_file(userid,mac_client,mac_server)
-  if(result == 0) then
-    return cjson.encode({userid=userid,client=mac_client})
-  else
-    return result
-  end
+  local server = data.server
+  local token  = data.token
+  local result = request_server(server,ip_client,token)
+  return result
 end
 
 function get_phone_mac_from_record_file_by_userid(userid)
@@ -200,9 +94,11 @@ end
 
 function parse_data_from_pad(data,env)
 	local ip_client = env.REMOTE_HOST
-	local mac_server = get_mac_by_ip(ip_client) -- 在这里为平板mac地址
-  syslog("mac_server: "..mac_server)
   syslog("ip client: "..ip_client)
+  syslog("ip_client="..ip_client)
+	--local mac_server = get_mac_by_ip(ip_client) -- 在这里为平板mac地址
+  --syslog("mac_server: "..mac_server)
+  
   local userid    = data.userid
   
 	if(data.action == 0) then --用户扫二维码时，平板发出消息(消息中有client地址则从黑名单中移除,主要是为了将用户ID和平板mac地址记录下来)
@@ -242,8 +138,8 @@ function parse_data_from_pad(data,env)
         return result_dis
   elseif (data.action == 2) then --平板向路由器请求获取wifi的SSID及密码
         syslog("type=0,action=2")
-        local cmd_get_wifi_ssid = "uci get wireless.default_radio0.ssid"
-        local cmd_get_wifi_key = "uci get wireless.default_radio0.key"
+        local cmd_get_wifi_ssid = "uci get wireless.default_ra.ssid"
+        local cmd_get_wifi_key = "uci get wireless.default_ra.key"
         
         local result_ssid = sys.exec(cmd_get_wifi_ssid)
         local result_key  = sys.exec(cmd_get_wifi_key)
@@ -258,13 +154,18 @@ function handle_request(env)
  
 	local client_data = io.read("*all")
 	local data = cjson.decode(client_data)
+
 	if(data.type == 0) then
-    syslog("data.type==0")
+    syslog("type="..data.type)
+    syslog("action="..data.action)
 		local result = parse_data_from_pad(data,env)
+    syslog("result="..result)
     uhttpd.send(result)
   elseif(data.type == 1) then
+    syslog("token ="..data.token)
     local result = parse_data_from_phone(data,env)
-    uhttpd.send(result)
+    local res  = {err=tostring(result)}
+    uhttpd.send(cjson.encode(res))
   end
 end
 
